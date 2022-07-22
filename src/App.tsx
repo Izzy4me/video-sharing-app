1418d3bb7e3ac381b8cb2647af923a340844bec3
Mateusz Smet <mateusz.smet@gmail.com>
2022-07-22 09:49:10 +0200

    Add basic structure on main view

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
+
+const Container = styled.div``;
+const Main = styled.div``;
+const Wrapper = styled.div``;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Menu />
+      <Main>
+        <Navbar />
+        <Wrapper>
+          There will be some videos
+        </Wrapper>
+      </Main>
+    </Container>
   );
 }
 
---
+++ src/components/Menu.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Menu = () => {
+  return (
+      <div>Menu placeholder</div>
+  );
+}
+
+export default Menu;
